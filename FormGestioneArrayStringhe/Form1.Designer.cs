a641d426888856eba88cc4e69b2807a056c0361c
thomasrota <rota.thomas.studente@itispaleocapa.it>
2023-02-10 17:59:41 +0100

    <aggiunta funzione 'Aggiunta'>

--- FormGestioneArrayStringhe/Form1.Designer.cs
+++ FormGestioneArrayStringhe/Form1.Designer.cs
@@ -28,13 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Titolo = new System.Windows.Forms.Label();
+            this.inpt = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.ext = new System.Windows.Forms.Button();
+            this.listview = new System.Windows.Forms.ListView();
+            this.selopz = new System.Windows.Forms.Label();
+            this.agg = new System.Windows.Forms.Button();
+            this.canc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Titolo
+            // 
+            this.Titolo.AutoSize = true;
+            this.Titolo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titolo.Location = new System.Drawing.Point(13, 13);
+            this.Titolo.Name = "Titolo";
+            this.Titolo.Size = new System.Drawing.Size(187, 18);
+            this.Titolo.TabIndex = 0;
+            this.Titolo.Text = "Gestione Stringhe Array";
+            // 
+            // inpt
+            // 
+            this.inpt.AutoSize = true;
+            this.inpt.Location = new System.Drawing.Point(16, 50);
+            this.inpt.Name = "inpt";
+            this.inpt.Size = new System.Drawing.Size(34, 13);
+            this.inpt.TabIndex = 1;
+            this.inpt.Text = "Input:";
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(19, 67);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(146, 20);
+            this.input.TabIndex = 2;
+            // 
+            // ext
+            // 
+            this.ext.Location = new System.Drawing.Point(713, 415);
+            this.ext.Name = "ext";
+            this.ext.Size = new System.Drawing.Size(75, 23);
+            this.ext.TabIndex = 3;
+            this.ext.Text = "Uscita";
+            this.ext.UseVisualStyleBackColor = true;
+            this.ext.Click += new System.EventHandler(this.ext_Click);
+            // 
+            // listview
+            // 
+            this.listview.HideSelection = false;
+            this.listview.Location = new System.Drawing.Point(351, 67);
+            this.listview.Name = "listview";
+            this.listview.Size = new System.Drawing.Size(415, 195);
+            this.listview.TabIndex = 4;
+            this.listview.UseCompatibleStateImageBehavior = false;
+            this.listview.View = System.Windows.Forms.View.List;
+            // 
+            // selopz
+            // 
+            this.selopz.AutoSize = true;
+            this.selopz.Location = new System.Drawing.Point(16, 109);
+            this.selopz.Name = "selopz";
+            this.selopz.Size = new System.Drawing.Size(92, 13);
+            this.selopz.TabIndex = 5;
+            this.selopz.Text = "Seleziona opzioni:";
+            // 
+            // agg
+            // 
+            this.agg.Location = new System.Drawing.Point(19, 136);
+            this.agg.Name = "agg";
+            this.agg.Size = new System.Drawing.Size(89, 23);
+            this.agg.TabIndex = 6;
+            this.agg.Text = "Aggiunta";
+            this.agg.UseVisualStyleBackColor = true;
+            this.agg.Click += new System.EventHandler(this.agg_Click);
+            // 
+            // canc
+            // 
+            this.canc.Location = new System.Drawing.Point(19, 166);
+            this.canc.Name = "canc";
+            this.canc.Size = new System.Drawing.Size(89, 23);
+            this.canc.TabIndex = 7;
+            this.canc.Text = "Cancella";
+            this.canc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.canc);
+            this.Controls.Add(this.agg);
+            this.Controls.Add(this.selopz);
+            this.Controls.Add(this.listview);
+            this.Controls.Add(this.ext);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.inpt);
+            this.Controls.Add(this.Titolo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Titolo;
+        private System.Windows.Forms.Label inpt;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button ext;
+        private System.Windows.Forms.ListView listview;
+        private System.Windows.Forms.Label selopz;
+        private System.Windows.Forms.Button agg;
+        private System.Windows.Forms.Button canc;
     }
 }
 
--- FormGestioneArrayStringhe/Form1.cs
+++ FormGestioneArrayStringhe/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,41 @@ namespace FormGestioneArrayStringhe
 {
     public partial class Form1 : Form
     {
+        // Dichiarazioni
+        public string[] array;                                                          // Dichiarazione array di tipo stringa 'array'
+        public string e;                                                                // Dichiarazione variabile di tipo stringa 'e'
+        public int dim;                                                                 // Dichiarazione variabile di tipo intero 'dim'
+        public int scelta;                                                              // Dichiarazione variabile di tipo intero 'scelta'
+
+        #region Funzioni Evento
         public Form1()
         {
             InitializeComponent();
+            dim = 0;                                                                    // Inizializzazione variabile 'dim'
+            array = new string[100];                                                    // Inizializzazione variabile 'array'
         }
+        private void agg_Click(object sender, EventArgs e)
+        {
+            Aggiunta(input.Text, array, ref dim);
+            input.Text = "";
+        }
+        private void ext_Click(object sender, EventArgs e)                              // Uscita programma
+        {
+            Application.Exit();
+        }
+        #endregion Funzioni Evento
+
+        #region Funzioni Servizio
+        public void Aggiunta(string e, string[] array, ref int dim)                   // Funzione 'Aggiunta' per aggiungere elementi all'array
+        {
+            if (dim < array.Length)                                                   // Se è stata raggiunta la dimensione massima dell'array           
+            {
+                array[dim] = e;                                                       // Aggiungere l'elemento
+                listview.Items.Add(array[dim]);                                      // Aggiungere l'elemento alla listview
+                dim++;                                                                // Incremento indice
+            }
+        }
+        #endregion Funzioni servizio
+
     }
 }
